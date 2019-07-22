3c1ab52bf68dde4e9123a724793d5724ee5e4730
dannydf123 <52668676+dannydf123@users.noreply.github.com>
2019-07-22 08:37:52 -0500

    Add files via upload

---
+++ assets/funciones/funciones.js
@@ -0,0 +1,16 @@
+ 
+ var numero = 5
+
+ otraVariable = 0.6
+ 
+
+ function funcionista () {
+
+var saludo = "Hola calidades"
+document.getElementById ("elSaludo").innerHTML = saludo
+
+ }
+
+
+ document.getElementById ("flotante").innerHTML= otraVariable
+ funcionista()
